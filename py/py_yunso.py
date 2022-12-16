83f50590a7ae7fd8aabea60997c4eb46715a254c
kebedd69 <105383277+kebedd69@users.noreply.github.com>
2022-12-16 15:05:40 +0800

    Add files via upload

--- py/py_yunso.py
+++ py/py_yunso.py
@@ -32,15 +32,14 @@ class Spider(Spider):
 		return result
 
 	header = {
-		"User-Agent": "Mozilla/5.0 (Linux; Android 12; V2049A Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36",
-		"origin": "https://www.upyunso.com/"
+		"User-Agent": "Mozilla/5.0 (Linux; Android 12; V2049A Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36"
 	}
 
 	def detailContent(self,array):
 		return self.ali.detailContent(array)
 
 	def searchContent(self,key,quick):
-		url = "https://api.upyunso.com/search?keyword={0}&page=1&s_type=2".format(key)
+		url = "https://upapi.juapp9.com/search?keyword={0}&page=1&s_type=2".format(key)
 		rsp = requests.get(url=url, headers=self.header)
 		vodList = json.loads(base64.b64decode(rsp.text))['result']['items']
 		videos = []
@@ -54,10 +53,10 @@ class Spider(Spider):
 					"vod_pic": "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000",
 					"vod_remarks": vod['insert_time']
 				})
-
 		result = {
-			'list':videos
+			'list': videos
 		}
+
 		return result
 
 	def playerContent(self,flag,id,vipFlags):
